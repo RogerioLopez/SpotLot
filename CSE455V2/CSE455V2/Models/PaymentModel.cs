f9c5937a698029182efc20ff750e1742026b916c
RogerioLopez <79489021+RogerioLopez@users.noreply.github.com>
2021-06-06 16:51:34 -0700

    Add files via upload

---
+++ CSE455V2/CSE455V2/Models/PaymentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE455V2.Models
+{
+    public class PaymentModel
+    {
+        public int studentID { get; set; }
+        public string cardNo { get; set; }
+        public string userName { get; set; }
+        public string cardHolderName { get; set; }
+        public string expDate { get; set; }
+        public string securityCode { get; set; }
+        public string NameBilling { get; set; }
+        public string streetAdressBilling { get; set; }
+        public string billingZipCode { get; set; }
+        public string billingState { get; set; }
+        public string billingCity { get; set; }
+
+    }
+}
---
+++ CSE455V2/CSE455V2/Models/UserInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE455V2.Models
+{
+    public enum AccountStatus
+    {
+        unlcoked, locked
+    }
+    public enum AccountType
+    {
+        student = 0, 
+        faculty = 1, 
+        security = 2
+    }
+    public class Users
+    {
+
+        private AccountStatus status;
+        //private AccountType accuntType = AccountType.student;
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public string CarYear { get; set; }
+        public string CarColor { get; set; }
+        public string LicenseNumber { get; set; }
+        public int NumberOfCitations { get; set; }
+        public AccountStatus SetAccountStatus
+        {
+            get
+            {
+                return status;
+            }
+            set 
+            {
+                    status = value;
+            }
+        }
+        public AccountType SetAccountType { get; set; }
+    }
+}
